da955d99c755c39f16b71d91b8d1e2976442f790
misho556 <misho556@abv.bg>
2015-07-17 10:01:19 +0300

    CI up

---
+++ application/controllers/home.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+
+	// public function __construct(){
+	// 	var_dump(STEAM_GAME_ID);die;
+	// }
+	
+	public function index()
+	{
+		$this->load->view('csgo');
+
+
+	}
+	
+    }
---
+++ application/views/csgo.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?php echo HTTP_SITE_URL ?>CS-GO Boosting</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo HTTP_CSS_URL ?>bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="<?php echo HTTP_CSS_URL ?>grayscale.css" rel="stylesheet">
+    <link href="<?php echo HTTP_CSS_URL ?>csgo.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="<?php echo HTTP_CSS_URL ?>font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    <i class="fa fa-gamepad"></i>  <span class="light">CS-GO</span> Bulgaria Boosting
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#download">Top boosters</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contacts</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h1 class="brand-heading">CS-GO Boosting</h1>
+                        <p class="intro-text">First Bulgarian CS-GO boosting platform. <br></p>
+                        <a href="#about" class="btn btn-circle page-scroll">
+                            <i class="fa fa-angle-double-down animated"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- About Section -->
+    <section id="about" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>About Bulgaria Boosting</h2>
+               CS:GO Bulgarian Boosting is web site wich will help you to get out off the rank you are stuck in.We will have at least 10 boosters.Every devision will cost you
+               different prices.Down you will see our top 3 boosters.Your keys/skins will be saved in SteamBot wich won't give them to the booster untill the boost is fully 
+               completed.Our boosters are bulgarians.They are honest and will take you out!</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Download Section -->
+   <section id="download" class="container" >
+
+   
+
+<ul id="menu">
+  <li><a href="/html/default.asp">1 Strike</a></li>
+  <li><a href="/css/default.asp">2 NoFlunx|10th Dan|</a></li>
+  <li><a href="/js/default.asp">3 Gosho </a></li>
+</ul>  
+
+  
+        <div >
+            <h2 class="boosterstitle">Top Boosters</h2>
+              <table class='table table-hover'>
+
+<style>
+table, th, td {
+    border: 1px solid white;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 15px;
+}
+</style>
+  <thead>
+      <tr>
+        <th>infomation</th>
+        <th>Strike</th>
+        <th>NoFlunx|10th Dan|</th>
+        <th>GOSHO</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr >
+        <td>reputation</td>
+        <td>0</td>
+        <td>0</td>
+        <td>0</td>
+      </tr>
+      <tr >
+        <td>clients</td>
+        <td>0</td>
+        <td>0</td>
+        <td>0</td>
+      </tr>
+      <tr >
+        <td>happy clients</td>
+        <td>0</td>
+        <td>0</td>
+        <td>0</td>
+
+      
+
+        
+      </tr>
+    </tbody>
+  </table>
+        
+        </div>
+   
+    </section>
+
+    <!-- Contact Section -->
+    <<section id="contact" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Contacts</h2>
+                <p>If there is some kind of a problem, or you want to say how good are our favors, email us.</p>
+                <p><a href="mailto:boosting.bulgaria@abv.bg">boosting.bulgaria@abv.bg</a>
+                </p>
+                <ul class="list-inline banner-social-buttons">
+                    <li>
+                        <a href="https://www.facebook.com/BGboosting?ref=aymt_homepage_panel"
+                        target="_blank" class="btn btn-default btn-lg"><i class="fa fa-facebook-official fa-fw"></i> <span class="network-name">Facebook</span></a>
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/channel/UCnHPy9ezziHn8iMuNsdph9g" class="btn btn-default btn-lg"><i class="fa fa-youtube-play"></i> <span class="network-name">Youtube</span></a>
+                    </li>
+                    <li>
+                        <a href="https://plus.google.com/+Startbootstrap/posts" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section >
+
+    
+    <div class="contact"></div>
+
+    
+    <footer>
+        <div class="container text-center">
+            <p>Copyright &copy; BugarianBoosting.com 2015</p>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="<?php echo HTTP_JS_URL ?>jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo HTTP_JS_URL ?>bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo HTTP_JS_URL ?>jquery.easing.min.js"></script>
+
+    <!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="<?php echo HTTP_JS_URL ?>grayscale.js"></script>
+
+</body>
+
+</html>
