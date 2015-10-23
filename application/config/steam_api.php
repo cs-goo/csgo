a2ba4c4ba0b405b56d667c6d9678f26dde72a93a
Simone <chaufnet@gmail.com>
2015-10-23 17:25:34 +0200

    Remove API key.

--- application/config/steam_api.php
+++ application/config/steam_api.php
@@ -1,8 +1,8 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 // Steam API - http://steamcommunity.com/dev/
-$config['steam_api_key'] = '1835C24C63FB78A43DC08FA01752A960'; // http://steamcommunity.com/dev/apikey
+$config['steam_api_key'] = ''; // http://steamcommunity.com/dev/apikey
 $config['steam_sign_in_button'] = 'border'; // border, no-border, small
 
-//1835C24C63FB78A43DC08FA01752A960
+//
 //steam_api_key
